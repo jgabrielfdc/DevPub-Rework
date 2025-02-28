a3184c768abe65cd2a6d8917d0bd7717a3b9a975
JGabrielFDC <151442605+jgabrielfdc@users.noreply.github.com>
2025-02-28 10:21:45 +0000

    :wrench: Correção Autoload

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'miniframework/miniframework',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '55e8b1cf39e59b650428e62f36e9d69a59ea5e3d',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3c864ee9ee170d7b44172b468f472c37cbf6e8f9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'miniframework/miniframework' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '55e8b1cf39e59b650428e62f36e9d69a59ea5e3d',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3c864ee9ee170d7b44172b468f472c37cbf6e8f9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
